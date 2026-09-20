2f76359ae3fc06ac2596280414072b8f79deb3e8
aalsaeed.trainee <aalsaeed.trainee@IT-TRAINEE47-PC>
2026-09-20 07:16:47 +0300

    Add project files.

---
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; } 
+    }
+}
